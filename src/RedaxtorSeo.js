49a560d7e10d693d5722814b800bff759d27c12a
Aleksey Alifer <aleksei.alifer@spiralscout.com>
2017-04-13 17:18:52 +0300

    fix comment from github

--- src/RedaxtorSeo.js
+++ src/RedaxtorSeo.js
@@ -277,5 +277,5 @@ RedaxtorSeo.__renderType = "BEFORE";
 RedaxtorSeo.__editLabel = i18n.__floatingEditLabel;
 RedaxtorSeo.__name = i18n.__checkboxName;
 RedaxtorSeo.applyEditor = function(node, data){
-
+    throw new Error("SEO editor data can't be applied to DOM node, update that data on server side");
 };
